979a1670b553c6f08540e03dd4e13c20d6593ce5
shyam <shyamjithsmaniyoor@gmail.com>
2019-12-08 22:46:01 +0530

    setting added for db connection with docker variable

--- app/app/settings.py
+++ app/app/settings.py
@@ -73,8 +73,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.environ.get('DB_HOST'),  # Mention The Docker File
+        'NAME': os.environ.get('DB_NAME'),  # Mention The Docker File
+        'USER': os.environ.get('DB_USER'),  # Mention The Docker File
+        'PASSWORD': os.environ.get('DB_PASS'),  # Mention The Docker File
+
         }
     }
 
